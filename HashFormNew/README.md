280a175f2238f4e16ac7739b2c916bb7a22afa3a
Igor Grešovnik <gresovnik@gmail.com>
2022-08-04 13:42:51 +0200

    Initial commit.

---
+++ HashFormNew/README.md
@@ -0,0 +1,14 @@
+# About the new HashForm
+
+
+This is reimplementation of IGLib's HashForm, a user friendly application for computation of various checksums or cryptographic hash functions of files an text. The original software is available here:
+* *https://www.softpedia.com/get/Others/Miscellaneous/HashForm.shtml*
+* *http://www2.arnes.si/~ljc3m2/igor/software/IGLibShellApp/HashForm.html*
+
+Currently implemented features include:
+* Main window
+
+Other functionality of the original app is planned in the future. The intention is to provide a cross-platform application. Linux application will depend on the availability of the .NET MAUI implementation on that platform (the original application referenced above should run on Linux with Mono framework installed).
+
+Copyright (c) Igor Grešovnik
+This repository is part of the Investigative Generic Library (IGLib). See license for terms of use.
---
+++ HashFormNew/src/HashFormNew/ViewModel/MainViewModel.cs
@@ -0,0 +1,119 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace MauiApp2.ViewModel;
+
+public partial class MainViewModel : 
+    ObservableObject
+    ,
+    INotifyPropertyChanged
+{
+
+
+    // event PropertyChangedEventHandler PropertyChanged;
+
+    private PropertyChangedEventHandler _propertyChanged;
+
+    event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+    {
+        add
+        {
+            _propertyChanged += value;
+        }
+        remove
+        {
+            _propertyChanged -= value;
+        }
+    }
+
+
+
+    public MainViewModel()
+    {
+        LaunchInfoDialogCommand = new Command(
+            execute: () =>
+            {
+                // Do the stuff...
+                // ToDo: implement command body!
+                // 
+                ((Command)LaunchInfoDialogCommand).ChangeCanExecute();
+            },
+            // ToDo: replace criterion below.
+            canExecute: () => Number < 20000.0
+        );
+
+    }
+
+    public ICommand LaunchInfoDialogCommand { get; private set; }
+
+    double number = 1;
+
+    public double Number
+    {
+        get
+        {
+            return number;
+        }
+        set
+        {
+            if (number != value)
+            {
+                number = value;
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Number"));
+            }
+        }
+    }
+
+
+
+
+
+    IConnectivity connectivity;
+    public MainViewModel(IConnectivity connectivity)
+    {
+        Items = new ObservableCollection<string>();
+        this.connectivity = connectivity;
+    }
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+    async Task Add()
+    {
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+
+        if(connectivity.NetworkAccess != NetworkAccess.Internet)
+        {
+            await Shell.Current.DisplayAlert("Uh Oh!", "No Internet", "OK");
+            return;
+        }
+
+        Items.Add(Text);
+        // add our item
+        Text = string.Empty;
+    }
+
+    [RelayCommand]
+    void Delete(string s)
+    {
+        if(Items.Contains(s))
+        {
+            Items.Remove(s);
+        }
+    }
+
+    [RelayCommand]
+    async Task Tap(string s)
+    {
+        await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+    }
+
+}
